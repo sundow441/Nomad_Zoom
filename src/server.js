dc8043ee196c965c99765c9795b9e7efa286216c
sundow441 <sundow441@hanmail.net>
2022-12-30 23:44:29 +0900

    Feat: Add WebSocket Messages (~1.4강) 서버: 브라우저로부터의 이벤트 별 listener 구현, 브라우저에서 보낸 메시지를 수신할 수 있도록 함 브라우저: 서버로부터의 이벤트 별 listener 구현, 서버에서 보낸 메시지를 수신할 수 있도록 함 (브라우저에서 서버로 연결을 만들고 서버는 connection을 기다림)

--- src/server.js
+++ src/server.js
@@ -16,7 +16,12 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (socket) => {
-  console.log(socket);
+  console.log("Connected to Browser✅");
+  socket.on("close", () => console.log("Disconnected from the Browser❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
